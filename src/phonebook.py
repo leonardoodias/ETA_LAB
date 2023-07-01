626a7f67e31c928bdd7ad0f44a446d9a575cf95e
leonardoodias <leonadias@gmail.com>
2023-07-01 13:09:37 -0300

    Mudanças na classe phonebook Criação dos testes Manter bkp do arquivo orginal phonebook_original.py

--- src/phonebook.py
+++ src/phonebook.py
@@ -1,10 +1,10 @@
-
 class Phonebook:
     """
     Classe Phonebook
     """
+
     def __init__(self):
-        #Inicializa a agenda telefônica com uma entrada predefinida
+        # Inicializa a agenda telefônica com uma entrada predefinida
         self.entries = {'POLICIA': '190'}
 
     def add(self, name, number):
@@ -14,27 +14,7 @@ class Phonebook:
         :param name: Nome da pessoa em formato de string
         :param number: Número de telefone da pessoa em formato de string
         :return: 'Nome inválido', 'Número inválido', 'Número adicionado' ou 'Nome duplicado'
-
-        if '#' in name:
-            return 'Nome invalido'
-        if '@' in name:
-            return 'Nme invalido'
-        if '!' in name:
-            return 'Nome invalido'
-        if '$' in name:
-            return 'Nome invalio'
-        if '%' in name:
-            return 'Nome invalido'
-
-        if len(number) < 0:
-            return 'Numero invalid'
-
-        if name not in self.entries:
-            self.entries[name] = number
-
-        return 'Numero adicionado'
         """
-
         invalid_chars = ['#', '@', '!', '$', '%']
         for char in invalid_chars:
             if char in name:
@@ -44,7 +24,7 @@ class Phonebook:
             return 'Número inválido'
 
         if name not in self.entries:
-        #Adiciona o nome e número fornecidos à agenda telefônica
+            # Adiciona o nome e número fornecidos à agenda telefônica
             self.entries[name] = number
             return 'Número adicionado'
         else:
@@ -52,26 +32,13 @@ class Phonebook:
 
     def lookup(self, name):
         """
-         Busca o número de telefone de uma pessoa pelo nome.
+        Busca o número de telefone de uma pessoa pelo nome.
+
         :param name: Nome da pessoa em formato de string
         :return: Número de telefone da pessoa ou 'Nome não encontrado'
-
-         if '#' in name:
-            return 'Nome invaldo'
-        if '@' in name:
-            return 'Nome invalido'
-        if '!' in name:
-            return 'Nme invalido'
-        if '$' in name:
-            return 'Nome invalido'
-        if '%' in name:
-            return 'Nome nvalido'
-
-        return self.entries[name]
         """
-
         if name in self.entries:
-        #Retorna o número de telefone correspondente ao nome fornecido
+            # Retorna o número de telefone correspondente ao nome fornecido
             return self.entries[name]
         else:
             return 'Nome não encontrado'
@@ -79,6 +46,7 @@ class Phonebook:
     def get_names(self):
         """
         Retorna uma lista com todos os nomes presentes na agenda telefônica.
+
         :return: Lista de nomes
         """
         return list(self.entries.keys())
@@ -86,6 +54,7 @@ class Phonebook:
     def get_numbers(self):
         """
         Retorna uma lista com todos os números presentes na agenda telefônica.
+
         :return: Lista de números
         """
         return list(self.entries.values())
@@ -93,7 +62,8 @@ class Phonebook:
     def clear(self):
         """
         Remove todas as entradas da agenda telefônica.
-       :return: Mensagem de confirmação 'Phonebook limpado'
+
+        :return: Mensagem de confirmação 'Phonebook limpado'
         """
         self.entries = {}
         return 'Phonebook limpado'
@@ -101,6 +71,7 @@ class Phonebook:
     def search(self, search_name):
         """
         Busca todas as entradas da agenda telefônica que contenham uma substring fornecida.
+
         :param search_name: Substring a ser pesquisada
         :return: Lista de resultados da pesquisa (dicionários contendo nome e número)
         """
@@ -113,6 +84,7 @@ class Phonebook:
     def get_phonebook_sorted(self):
         """
         Retorna a agenda telefônica em ordem alfabética.
+
         :return: Agenda telefônica ordenada
         """
         return dict(sorted(self.entries.items()))
@@ -120,15 +92,20 @@ class Phonebook:
     def get_phonebook_reverse(self):
         """
         Retorna a agenda telefônica em ordem alfabética inversa.
+
         :return: Agenda telefônica em ordem reversa
         """
         return dict(sorted(self.entries.items(), reverse=True))
 
     def delete(self, name):
         """
-        Delete person with name
-        :param name: String with name
-        :return: return 'Numero deletado'
+        Deleta uma pessoa pelo nome.
+
+        :param name: Nome da pessoa a ser deletada
+        :return: 'Número deletado' ou 'Nome não encontrado'
         """
-        self.entries.pop(name)
-        return 'Numero deletado'
+        if name in self.entries:
+            del self.entries[name]
+            return 'Número deletado'
+        else:
+            return 'Nome não encontrado'
--- test/test_phonebook.py
+++ test/test_phonebook.py
@@ -2,164 +2,163 @@ from src.phonebook import Phonebook
 
 class TestPhonebook:
 
-    def test_add_sucess(self):
-    # Testa a adição de um número de tefefone válido
-       phone = Phonebook()
-       result = phone.add('Joao', '123456789')
-       expected = 'Numero adicionado'
-       assert result == expected
+    def test_add_success(self):
+        # Testa a adição de um número de telefone válido
+        phone = Phonebook()
+        result = phone.add('Joao', '123456789')
+        expected = "Número adicionado"
+        assert result == expected
 
-   def test_add_invalid_name(self):
-    # Testa a adição de um nome inválido
-       phone = Phonebook()
-       result = phone.add('Joao#', '123456789')
-       expected = 'Nome invalido'
-       assert result == expected
+    def test_add_invalid_name(self):
+        # Testa a adição de um nome inválido
+        phone = Phonebook()
+        result = phone.add('Joao#', '123456789')
+        expected = 'Nome inválido'
+        assert result == expected
 
-   def test_add_invalid_number(self):
-    # Testa a adição de um número de telefone inválido
-       phone = Phonebook()
-       result = phone.add('Joao', '')
-       expected = 'Numero invalido'
-       assert result == expected
+    def test_add_invalid_number(self):
+        # Testa a adição de um número de telefone inválido
+        phone = Phonebook()
+        result = phone.add('Joao', '')
+        expected = 'Número inválido'
+        assert result == expected
 
-   def test_add_invalid_name_character(self):
-       # Testa a adição de um nome inválido com caracteres especiais
-       phone = Phonebook()
-       expected = 'Nome invalido'
-       character = ['Joao!', 'Joa@', 'Joa$o', 'Joao%', 'J#oao']
-       for name in character:
-           result = phone.add(name, '123456789')
-           assert result == expected
+    def test_add_invalid_name_character(self):
+        # Testa a adição de um nome inválido com caracteres especiais
+        phone = Phonebook()
+        expected = 'Nome inválido'
+        characters = ['Joao!', 'Joa@', 'Joa$o', 'Joao%', 'J#oao']
+        for name in characters:
+            result = phone.add(name, '123456789')
+            assert result == expected
 
-   def test_get_names(self):
-       # Testa a obtenção de nomes do telefone
-       phone = Phonebook()
-       expected = ['POLICIA']
-       result = phone.get_names()
-       assert result == expected
+    def test_get_names(self):
+        # Testa a obtenção de nomes do telefone
+        phone = Phonebook()
+        expected = ['POLICIA']
+        result = phone.get_names()
+        assert result == expected
 
-   def test_get_numbers(self):
-       # Testa a obtenção de números do telefone
-       phone = Phonebook()
-       expected = ['190']
-       result = phone.get_numbers()
-       assert result == expected
+    def test_get_numbers(self):
+        # Testa a obtenção de números do telefone
+        phone = Phonebook()
+        expected = ['190']
+        result = phone.get_numbers()
+        assert result == expected
 
-   def test_clear(self):
-       # Testa a limpeza do telefone
-       phone = Phonebook()
-       phone.add('Joao', '123456789')
-       expected = 'phonebook clear'
-       result = phone.clear()
-       assert result == expected
+    def test_clear(self):
+        # Testa a limpeza do telefone
+        phone = Phonebook()
+        phone.add('Joao', '123456789')
+        expected = 'Phonebook limpado'
+        result = phone.clear()
+        assert result == expected
 
-   def test_search_sucess(self):
-       /// Testa a busca de um nome no telefone
-       phone = Phonebook()
-       phone.add('Joao', '123456789')
-       expected = 'Joao'
-       result = phone.search('Joao')
-       assert result == expected
+    def test_search_success(self):
+        # Testa a busca de um nome no telefone
+        phone = Phonebook()
+        phone.add('Maria', '123456789')
+        expected = [{'Maria': '123456789'}]
+        result = phone.search('Maria')
+        assert result == expected
 
-   def test_search_sucess2(self):
-       #testa a busca de um nome no telefone
-       phone = Phonebook()
-       expected = 'Leonardo'
-       list = ['Leonardo', 'Joao', 'Maria']
-       for name, number in list.items():
-           phone.add(name, number)
+    def test_search_success2(self):
+        # Testa a busca de um nome no telefone
+        phone = Phonebook()
+        expected = [{'Leonardo': '123456'}]
+        entries = {'Leonardo': '123456', 'Filipe': '123456789', 'Rosa': '123456789'}
+        for name, number in entries.items():
+            phone.add(name, number)
 
-       result = phone.search('Leonardo')
-       assert result == expected
+        result = phone.search('Leonardo')
+        assert result == expected
 
-   def test_search_not_found(self):
-       # Testa a busca de um nome que não existe no telefone
-       phone = Phonebook()
-       expected = 'Nome nao encontrado'
-       result = phone.search('Leonardo')
-       assert result == expected
+    def test_search_not_found(self):
+        # Testa a busca de um nome que não existe no telefone
+        phone = Phonebook()
+        expected = []
 
-   def test_search_not_found2(self):
-       # Testa a busca de um nome que não existe no telefone
-       phone = Phonebook()
-       expected = 'Contato nao encontrado'
-       list = {'Leonardo': '123456', 'Joao': '123456789', 'Maria': '123456789'}
-       for name, number in list.items():
-           phone.add(name, number)
+        result = phone.search('Giovani')
+        assert result == expected
 
-       result = phone.search('Goku')
-       assert result == expected
+    def test_search_not_found2(self):
+        # Testa a busca de um nome que não existe no telefone
+        phone = Phonebook()
+        expected = []
+        contact_list = {'Leonardo': '123456', 'Joao': '123456789', 'Maria': '123456789'}
+        for name, number in contact_list.items():
+            phone.add(name, number)
+        result = phone.search('Goku')
+        assert result == expected
 
-   def test_get_phonebook_sorted(self):
-       # testa a ordenação do telefone
-       phone = Phonebook()
-       expected = {'POLICIA': '190'}
-       result = phone.get_phonebook_sorted()
-       assert result == expected
+    def test_get_phonebook_sorted(self):
+        # Testa a ordenação do telefone
+        phone = Phonebook()
+        expected = {'POLICIA': '190'}
+        result = phone.get_phonebook_sorted()
+        assert result == expected
 
-   def test_get_phonebook_sorted2(self):
-       # testa a ordenação do telefone
-       phone = Phonebook()
-       expected = {'POLICIA': '190', 'BOMBEIROS': '193'}
-       phone.add('BOMBEIROS', '193')
-       result = phone.get_phonebook_sorted()
+    def test_get_phonebook_sorted2(self):
+        # Testa a ordenação do telefone
+        phone = Phonebook()
+        expected = {'POLICIA': '190', 'BOMBEIROS': '193'}
+        phone.add('BOMBEIROS', '193')
+        result = phone.get_phonebook_sorted()
+        assert result == expected
 
-       assert result == expected
+    def test_get_phonebook_sorted3(self):
+        # Testa a ordenação do telefone
+        phone = Phonebook()
+        expected = {'POLICIA': '190', 'BOMBEIROS': '193', 'SAMU': '192'}
+        phone.add('BOMBEIROS', '193')
+        phone.add('SAMU', '192')
+        result = phone.get_phonebook_sorted()
+        assert result == expected
 
-   def test_get_phonebook_sorted3(self):
-       # testa a ordenação do telefone
-       phone = Phonebook()
-       expected = {'POLICIA': '190', 'BOMBEIROS': '193', 'SAMU': '192'}
-       phone.add('BOMBEIROS', '193')
-       phone.add('SAMU', '192')
-       result = phone.get_phonebook_sorted()
-       assert result == expected
+    def test_get_phonebook_sorted4(self):
+        # Testa a ordenação do telefone
+        phone = Phonebook()
+        expected = {'POLICIA': '190', 'BOMBEIROS': '193', 'SAMU': '192', 'JOAO': '123456789'}
+        phone.add('BOMBEIROS', '193')
+        phone.add('SAMU', '192')
+        phone.add('JOAO', '123456789')
+        result = phone.get_phonebook_sorted()
+        assert result == expected
 
-   def test_get_phonebook_sorted4(self):
-       # testa a ordenação do telefone
-       phone = Phonebook()
-       expected = {'POLICIA': '190', 'BOMBEIROS': '193', 'SAMU': '192', 'JOAO': '123456789'}
-       phone.add('BOMBEIROS', '193')
-       phone.add('SAMU', '192')
-       phone.add('JOAO', '123456789')
-       result = phone.get_phonebook_sorted()
-       assert result == expected
+    def test_get_phonebook_reverse(self):
+        # Testa a ordenação reversa do telefone
+        phone = Phonebook()
+        expected = {'POLICIA': '190'}
+        result = phone.get_phonebook_reverse()
+        assert result == expected
 
-   def test_get_phonebook_reverse(self):
-       # Testa a ordenação reversa do telefone
-       phone = Phonebook()
-       expected = {'POLICIA': '190'}
-       result = phone.get_phonebook_reverse()
-       assert result == expected
+    def test_get_phonebook_reverse2(self):
+        # Testa a ordenação reversa do telefone
+        phone = Phonebook()
+        expected = {'POLICIA': '190', 'BOMBEIROS': '193'}
+        phone.add('BOMBEIROS', '193')
+        result = phone.get_phonebook_reverse()
+        assert result == expected
 
-   def test_get_phonebook_reverse2(self):
-       # Testa a ordenação reversa do telefone
-       phone = Phonebook()
-       expected = {'POLICIA': '190', 'BOMBEIROS': '193'}
-       phone.add('BOMBEIROS', '193')
-       result = phone.get_phonebook_reverse()
-       assert result == expected
+    def test_delete_success(self):
+        # Testa a deleção de um contato
+        phone = Phonebook()
+        phone.add('Joao', '123456789')
+        expected = 'Número deletado'
+        result = phone.delete('Joao')
+        assert result == expected
 
-   def test_delete_sucess(self):
-       # Testa a deleção de um contato
-       phone = Phonebook()
-       phone.add('Joao', '123456789')
-       expected = 'Contato deletado'
-       result = phone.delete('Joao')
-       assert result == expected
+    def test_delete_contact_not_found(self):
+        # Testa a deleção de um contato que não existe
+        phone = Phonebook()
+        expected = 'Nome não encontrado'
+        result = phone.delete('Joao')
+        assert result == expected
 
-   def test_delete_contact_not_found(self):
-       #Testa a deleção de um contato que não existe
-       phone = Phonebook()
-       expected = 'Contato nao encontrado'
-       result = phone.delete('Joao')
-       assert result == expected
-
-   def test_delete_contact_not_found2(self):
-       # Testa a deleção de um contato que não existe
-       phone = Phonebook()
-       phone.add('Joao', '123456789')
-       expected = 'Contato nao encontrado'
-       result = phone.delete('Maria')
-       assert result == expected
+    def test_delete_contact_not_found2(self):
+        # Testa a deleção de um contato que não existe
+        phone = Phonebook()
+        phone.add('Joao', '123456789')
+        expected = 'Nome não encontrado'
+        result = phone.delete('Maria')
+        assert result == expected
